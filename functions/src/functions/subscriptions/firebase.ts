4cacca5502d1225d2caad4cca3b9bda66c6ca5a3
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-04-28 10:04:29 +0200

    parse firebase pem key

--- functions/src/functions/subscriptions/firebase.ts
+++ functions/src/functions/subscriptions/firebase.ts
@@ -8,7 +8,7 @@ firebase.initializeApp({
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY!),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
