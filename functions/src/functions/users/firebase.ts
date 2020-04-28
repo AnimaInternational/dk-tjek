3586069540fc466a80b790993be63e69f7fb1c2d
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-04-28 10:08:44 +0200

    update firebase configuration

--- functions/src/functions/users/firebase.ts
+++ functions/src/functions/users/firebase.ts
@@ -1,19 +1,32 @@
 import firebase from "firebase-admin";
 
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_CLIENT_X509_CERT_URL,
+  FIREBASE_DATABASE_URL,
+} = process.env;
+
 firebase.initializeApp({
   credential: firebase.credential.cert({
     type: "service_account",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY!),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    project_id: FIREBASE_PROJECT_ID,
+    private_key_id: FIREBASE_PRIVATE_KEY_ID,
+    private_key:
+      FIREBASE_PRIVATE_KEY![0] === "-"
+        ? FIREBASE_PRIVATE_KEY
+        : JSON.parse(FIREBASE_PRIVATE_KEY!),
+    client_email: FIREBASE_CLIENT_EMAIL,
+    client_id: FIREBASE_CLIENT_ID,
+    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
   } as any),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL: FIREBASE_DATABASE_URL,
 });
 
 export const firebaseAuth = firebase.auth();
