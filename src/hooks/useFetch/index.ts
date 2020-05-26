5f1256eb54f9e6bf1bc23275418130c4e2398ae4
Adam Alfredsson <alfredsson.adam@gmail.com>
2020-05-26 11:15:01 +0200

    fix useFetch hook

--- src/hooks/useFetch/index.ts
+++ src/hooks/useFetch/index.ts
@@ -1,21 +1,26 @@
 import { AxiosRequestConfig } from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "../../axios";
 
 export const useFetch = <T = any>(
   endpoint: string,
   options: AxiosRequestConfig = {}
 ) => {
+  const optionsRef = useRef(options);
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    optionsRef.current = options;
+  });
 
   useEffect(() => {
     async function getData() {
       try {
         setLoading(true);
         setError(null);
-        const response = await axios.get<T>(`${endpoint}`, options);
+        const response = await axios.get<T>(`${endpoint}`, optionsRef.current);
         setData(response.data);
       } catch (e) {
         setError(e);
@@ -24,7 +29,7 @@ export const useFetch = <T = any>(
       }
     }
     getData();
-  }, [endpoint, options]);
+  }, [endpoint]);
 
   return { data, error, loading };
 };
